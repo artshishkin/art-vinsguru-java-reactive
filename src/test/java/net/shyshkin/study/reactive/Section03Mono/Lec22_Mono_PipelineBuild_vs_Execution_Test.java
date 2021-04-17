e5b41cfd86520d5e7e76159cae845ebd107e7b61
Art <d.art.shishkin@gmail.com>
2021-04-17 14:58:31 +0300

    23. [Mono] - Where is Async? (#1)

--- src/test/java/net/shyshkin/study/reactive/Section03Mono/Lec22_Mono_PipelineBuild_vs_Execution_Test.java
+++ src/test/java/net/shyshkin/study/reactive/Section03Mono/Lec22_Mono_PipelineBuild_vs_Execution_Test.java
@@ -3,6 +3,7 @@ package net.shyshkin.study.reactive.Section03Mono;
 import net.shyshkin.study.reactive.courseutil.Util;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 public class Lec22_Mono_PipelineBuild_vs_Execution_Test {
 
@@ -19,13 +20,30 @@ public class Lec22_Mono_PipelineBuild_vs_Execution_Test {
     }
 
     @Test
-    void withSubscriber() {
+    void withSubscriber_blocking() {
 
         //when
-        Mono<String> mono = getName();
+        getName();
+        getName().subscribe(Util.onNext, Util.onError, Util.onComplete);
+        getName();
 
         //then
-        mono.subscribe(Util.onNext, Util.onError, Util.onComplete);
+        System.out.println("Subscriber method executes in main thread so it is blocking");
+    }
+
+    @Test
+    void withSubscriber_nonBlocking() {
+
+        //when
+        getName();
+        getName()
+                .subscribeOn(Schedulers.boundedElastic())
+                .subscribe(Util.onNext, Util.onError, Util.onComplete);
+        getName();
+
+        //then
+        System.out.println("Subscriber method executes in different thread so it is not blocking");
+        Util.sleep(1.2);
     }
 
     private Mono<String> getName() {
