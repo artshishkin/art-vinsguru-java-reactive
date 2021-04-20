500c24bca9e1604b71aefc6f86768a9f012ec69e
Art <d.art.shishkin@gmail.com>
2021-04-20 22:51:57 +0300

    85. Hot Publisher - Cache (#5)

---
+++ src/test/java/net/shyshkin/study/reactive/Section07HotAndColdPublishers/Lec85_HotPublisher_Cache_Test.java
@@ -0,0 +1,73 @@
+package net.shyshkin.study.reactive.Section07HotAndColdPublishers;
+
+import lombok.extern.slf4j.Slf4j;
+import net.shyshkin.study.reactive.courseutil.Util;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+@Slf4j
+public class Lec85_HotPublisher_Cache_Test {
+
+    @Test
+    @DisplayName("When shared flux by CACHE is in completed state new subscriber will receive all the missed items immediately")
+    void hotPublisher_cache() throws InterruptedException {
+        //given
+        log.debug("Start test");
+        // cache = publish().replay() - int.max
+        CountDownLatch latch = new CountDownLatch(2);
+        Flux<String> movieStream = Flux.fromStream(this::getMovie)
+                .delayElements(Duration.ofMillis(100))
+                .cache();
+
+        //when
+        Util.sleep(0.3);
+        movieStream
+                .subscribe(Util.subscriber("Sub1", latch));
+        Util.sleep(0.4);
+        log.debug("Sub2 is about to join stream");
+        movieStream
+                .subscribe(Util.subscriber("Sub2", latch));
+
+        //then
+        latch.await(2, TimeUnit.SECONDS);
+    }
+
+    @Test
+    @DisplayName("Cached flux with certain amount will give only last N missed items")
+    void hotPublisher_cache2() throws InterruptedException {
+        //given
+        log.debug("Start test");
+        // cache = publish().replay()
+        CountDownLatch latch = new CountDownLatch(2);
+        Flux<String> movieStream = Flux.fromStream(this::getMovie)
+                .delayElements(Duration.ofMillis(100))
+                .cache(2);
+
+        //when
+        Util.sleep(0.4);
+        movieStream
+                .subscribe(Util.subscriber("Sub1", latch));
+        Util.sleep(1);
+        log.debug("Sub2 is about to join stream");
+        movieStream
+                .subscribe(Util.subscriber("Sub2", latch));
+
+        //then
+        latch.await(2, TimeUnit.SECONDS);
+    }
+
+    //movie-theatre
+    private Stream<String> getMovie() {
+        log.debug("Got the movie streaming request");
+        return IntStream.rangeClosed(1, 7)
+                .mapToObj(i -> String.format("Scene %2d", i));
+    }
+
+}
