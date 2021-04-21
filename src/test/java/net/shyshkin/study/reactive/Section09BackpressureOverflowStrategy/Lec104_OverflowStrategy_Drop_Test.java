100caff5383ad928f6cde7872621b01e7f5651a6
Art <d.art.shishkin@gmail.com>
2021-04-21 15:54:50 +0300

    105. Overflow Strategy - Drop (#7)

---
+++ src/test/java/net/shyshkin/study/reactive/Section09BackpressureOverflowStrategy/Lec104_OverflowStrategy_Drop_Test.java
@@ -0,0 +1,66 @@
+package net.shyshkin.study.reactive.Section09BackpressureOverflowStrategy;
+
+import lombok.extern.slf4j.Slf4j;
+import net.shyshkin.study.reactive.courseutil.Util;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.concurrent.CountDownLatch;
+
+@Slf4j
+public class Lec104_OverflowStrategy_Drop_Test {
+
+    @Test
+    void drop() throws InterruptedException {
+        //given
+        CountDownLatch latch = new CountDownLatch(1);
+
+        Flux.create(
+                fluxSink -> {
+                    for (int i = 1; i < 501 && !fluxSink.isCancelled(); i++) {
+                        fluxSink.next(i);
+                        log.debug("Pushed: {}", i);
+                    }
+                    fluxSink.complete();
+                })
+
+                //when
+                .onBackpressureDrop()
+                .publishOn(Schedulers.boundedElastic())
+                .doOnNext(i -> Util.sleep(0.01))
+
+                //then
+                .subscribe(Util.subscriber(latch));
+
+        latch.await();
+    }
+
+    @Test
+    void drop_changeBufferSize() throws InterruptedException {
+        //given
+        CountDownLatch latch = new CountDownLatch(1);
+
+        System.setProperty("reactor.bufferSize.small", "16");
+
+        Flux.create(
+                fluxSink -> {
+                    for (int i = 1; i < 501 && !fluxSink.isCancelled(); i++) {
+                        fluxSink.next(i);
+                        log.debug("Pushed: {}", i);
+                        Util.sleep(0.001);
+                    }
+                    fluxSink.complete();
+                })
+
+                //when
+                .onBackpressureDrop()
+                .publishOn(Schedulers.boundedElastic())
+                .doOnNext(i -> Util.sleep(0.01))
+
+                //then
+                .subscribe(Util.subscriber(latch));
+
+        latch.await();
+    }
+}
