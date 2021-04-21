2cc3bc097c9734790d2aa28827d9ba5f7b085026
Art <d.art.shishkin@gmail.com>
2021-04-21 14:57:35 +0300

    98. Parallel Execution - Thread Safe Collections Test (#6)

--- src/test/java/net/shyshkin/study/reactive/Section8ThreadingSchedulers/Lec98_ParallelExecution_Test.java
+++ src/test/java/net/shyshkin/study/reactive/Section8ThreadingSchedulers/Lec98_ParallelExecution_Test.java
@@ -1,16 +1,20 @@
 package net.shyshkin.study.reactive.Section8ThreadingSchedulers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.ParallelFlux;
 import reactor.core.scheduler.Schedulers;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 public class Lec98_ParallelExecution_Test {
@@ -107,25 +111,75 @@ public class Lec98_ParallelExecution_Test {
         latch.await();
     }
 
-    @Test
-    void parallel_arrayList() throws InterruptedException {
-        //given
+    @Nested
+    class ThreadSafeListPlay {
+
         int count = 1000;
-        List<Integer> list = new ArrayList<>(count);
-        CountDownLatch latch = new CountDownLatch(1);
+        private CountDownLatch latch;
+        private ParallelFlux<Integer> flux;
 
-        //when
-        ParallelFlux<Integer> flux = Flux.range(1, count)
-                .parallel()
-                .runOn(Schedulers.parallel());
+        @BeforeEach
+        void setUp() {
 
-        //then
-        flux
-//                .doOnNext(i -> printThreadName("next:" + i))
-                .subscribe(list::add, null, latch::countDown);
+            latch = new CountDownLatch(1);
+            flux = Flux.range(1, count)
+                    .parallel()
+                    .runOn(Schedulers.parallel());
+        }
 
-        latch.await();
-        assertNotEquals(count, list.size());
+        @Test
+        void parallel_arrayList() throws InterruptedException {
+            //given
+            List<Integer> list = new ArrayList<>(count);
+
+            //when
+            flux
+                    .subscribe(list::add, null, latch::countDown);
+
+            latch.await();
+
+            //then
+            assertEquals(ArrayList.class, list.getClass());
+            assertNotEquals(count, list.size());
+        }
+
+        @Test
+        void parallel_synchronizedList() throws InterruptedException {
+            //given
+            List<Integer> list = new ArrayList<>(count);
+            List<Integer> synchronizedList = Collections.synchronizedList(list);
+
+            //when
+            flux
+                    .subscribe(synchronizedList::add, null, latch::countDown);
+
+            latch.await();
+
+            //then
+            assertAll(
+                    () -> assertEquals(ArrayList.class, list.getClass()),
+                    () -> assertEquals("SynchronizedRandomAccessList", synchronizedList.getClass().getSimpleName()),
+                    () -> assertEquals(count, list.size())
+            );
+        }
+
+        @Test
+        void parallel_copyOnWriteArrayList() throws InterruptedException {
+            //given
+            List<Integer> list = new CopyOnWriteArrayList<>();
+
+            //when
+            flux
+                    .subscribe(list::add, null, latch::countDown);
+
+            latch.await();
+
+            //then
+            assertAll(
+                    () -> assertEquals(CopyOnWriteArrayList.class, list.getClass()),
+                    () -> assertEquals(count, list.size())
+            );
+        }
     }
 
     private void printThreadName(String msg) {
