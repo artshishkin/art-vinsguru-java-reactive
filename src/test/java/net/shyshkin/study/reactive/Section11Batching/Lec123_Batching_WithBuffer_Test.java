8905380e8fda5f30f73594d2800092f3c1b9b4e3
Art <d.art.shishkin@gmail.com>
2021-04-22 16:08:42 +0300

    125. Batching With Buffer - Timeout (#9)

--- src/test/java/net/shyshkin/study/reactive/Section11Batching/Lec123_Batching_WithBuffer_Test.java
+++ src/test/java/net/shyshkin/study/reactive/Section11Batching/Lec123_Batching_WithBuffer_Test.java
@@ -63,8 +63,80 @@ public class Lec123_Batching_WithBuffer_Test {
         latch.await();
     }
 
+    @Test
+    void bufferedTimespan() throws InterruptedException {
+
+        //given
+        CountDownLatch latch = new CountDownLatch(1);
+        log.debug("Starting...");
+
+        //when
+        eventStream()
+
+                //then
+                .take(12)
+                .buffer(Duration.ofMillis(1100))
+                .subscribe(Util.subscriber(latch));
+
+        latch.await();
+    }
+
+    @Test
+    void bufferTimeout_randomInterval() throws InterruptedException {
+
+        //given
+        CountDownLatch latch = new CountDownLatch(1);
+        log.debug("Starting...");
+
+        //when
+        eventStreamRandomTime()
+
+                //then
+                .take(120)
+                .bufferTimeout(5, Duration.ofMillis(500))
+                .subscribe(Util.subscriber(latch));
+
+        latch.await();
+    }
+
+    @Test
+    void bufferTimeout_slowFast() throws InterruptedException {
+
+        //given
+        CountDownLatch latch = new CountDownLatch(1);
+        log.debug("Starting...");
+
+        //when
+        eventStreamSlowFast()
+
+                //then
+                .take(30)
+                .bufferTimeout(5, Duration.ofMillis(500))
+                .subscribe(Util.subscriber(latch));
+
+        latch.await();
+    }
+
     private Flux<String> eventStream() {
         return Flux.interval(Duration.ofMillis(300))
+                .map(i -> String.format("event %03d", i));
+    }
+
+    private Flux<String> eventStreamRandomTime() {
+        return Flux.generate(() -> 0, (tick, sink) -> {
+            sink.next(String.format("event %03d", tick));
+            Util.sleep(0.001 * Util.FAKER.random().nextInt(10, 300));
+            return tick + 1;
+        });
+    }
+
+    private Flux<String> eventStreamSlowFast() {
+        return Flux
+                .concat(
+                        Flux.interval(Duration.ZERO, Duration.ofMillis(300)).take(8),
+                        Flux.interval(Duration.ofMillis(1100)).take(3).map(i -> i + 10),
+                        Flux.interval(Duration.ZERO, Duration.ofMillis(30)).map(i -> i + 20)
+                )
                 .map(i -> String.format("event %03d", i));
     }
 
