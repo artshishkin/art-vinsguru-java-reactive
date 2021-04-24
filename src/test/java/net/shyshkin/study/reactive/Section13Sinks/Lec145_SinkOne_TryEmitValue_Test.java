f0013f66e0626d59432cc8e5284bc1b399e90beb
Art <d.art.shishkin@gmail.com>
2021-04-24 16:25:23 +0300

    146. Sink One - Emit Failure Handler (#11)

--- src/test/java/net/shyshkin/study/reactive/Section13Sinks/Lec145_SinkOne_TryEmitValue_Test.java
+++ src/test/java/net/shyshkin/study/reactive/Section13Sinks/Lec145_SinkOne_TryEmitValue_Test.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
+import reactor.core.scheduler.Schedulers;
 
 import java.time.Duration;
 import java.util.concurrent.CountDownLatch;
@@ -41,5 +42,124 @@ public class Lec145_SinkOne_TryEmitValue_Test {
         //then
         Util.sleep(1);
         sink.tryEmitValue("HI");
+    }
+
+    @Test
+    void sink_tryEmitEmpty() {
+        //given
+        //mono 1 value / empty / error
+        Sinks.One<Object> sink = Sinks.one();
+        log.debug("Start sink test");
+
+        //when
+        Mono<Object> mono = sink.asMono();
+        mono
+                .subscribe(Util.subscriber(latch));
+        //then
+        Util.sleep(1);
+        sink.tryEmitEmpty();
+    }
+
+    @Test
+    void sink_tryEmitError() {
+        //given
+        //mono 1 value / empty / error
+        Sinks.One<Object> sink = Sinks.one();
+        log.debug("Start sink test");
+
+        //when
+        Mono<Object> mono = sink.asMono();
+        mono
+                .subscribe(Util.subscriber(latch));
+        //then
+        Util.sleep(1);
+        sink.tryEmitError(new IllegalStateException("Some fake exception"));
+    }
+
+    @Test
+    void sink_emitValue() {
+        //given
+        //mono 1 value / empty / error
+        Sinks.One<Object> sink = Sinks.one();
+        log.debug("Start sink test");
+
+        //when
+        Mono<Object> mono = sink.asMono();
+        mono
+                .subscribe(Util.subscriber(latch));
+        //then
+        Util.sleep(1);
+
+        sink.emitValue(
+                "hi",
+                (signalType, emitResult) -> {
+                    log.debug("{}:{}:{}", signalType.name(), signalType, emitResult.name());
+                    return false; //retry?
+                }
+        );
+    }
+
+    @Test
+    void sink_emitValue_errorNoRetry() {
+        //given
+        //mono 1 value / empty / error
+        Sinks.One<Object> sink = Sinks.one();
+        log.debug("Start sink test");
+
+        //when
+        Mono<Object> mono = sink.asMono();
+        mono
+                .map(i -> i)
+                .subscribe(Util.subscriber(latch));
+        //then
+        Util.sleep(1);
+
+        sink.emitValue(
+                "hi",
+                (signalType, emitResult) -> {
+                    log.debug("{}:{}:{}", signalType.name(), signalType, emitResult.name());
+                    return false; //retry?
+                }
+        );
+        sink.emitValue(
+                "hello",
+                (signalType, emitResult) -> {
+                    log.debug("{}:{}:{}", signalType.name(), signalType, emitResult.name());
+                    return false; //retry?
+                }
+        );
+    }
+
+    @Test
+    void sink_emitValue_error_withForeverRetry() {
+        //given
+        //mono 1 value / empty / error
+        Sinks.One<Object> sink = Sinks.one();
+        log.debug("Start sink test");
+
+        //when
+        Mono<Object> mono = sink.asMono();
+        mono
+                .publishOn(Schedulers.boundedElastic())
+                .subscribe(Util.subscriber(latch));
+        //then
+        Util.sleep(1);
+
+        Runnable runnable = () ->
+                sink.emitValue(
+                        "hi from " + Thread.currentThread().getName(),
+                        (signalType, emitResult) -> {
+                            log.debug("{}:{}:{}", signalType.name(), signalType, emitResult.name());
+                            Util.sleep(0.1);
+                            return true; //retry?
+                        }
+                );
+
+        for (int i = 0; i < 2; i++) {
+            new Thread(runnable).start();
+            Util.sleep(0.01);
+        }
+
+        Util.sleep(2);
     }
 }
