ebd91a60e8e8ada42381115851701456fc1a3020
Art <d.art.shishkin@gmail.com>
2021-04-25 10:31:55 +0300

    152. Sink Many - Multicast (#11)

---
+++ src/test/java/net/shyshkin/study/reactive/Section13Sinks/Lec152_SinkMany_Multicast_Test.java
@@ -0,0 +1,57 @@
+package net.shyshkin.study.reactive.Section13Sinks;
+
+import lombok.extern.slf4j.Slf4j;
+import net.shyshkin.study.reactive.courseutil.Util;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.stream.IntStream;
+
+@Slf4j
+public class Lec152_SinkMany_Multicast_Test {
+
+    private CountDownLatch latch;
+
+    @BeforeEach
+    void setUp() {
+        latch = new CountDownLatch(1);
+    }
+
+    @AfterEach
+    void tearDown() throws InterruptedException {
+        latch.await();
+    }
+
+    @Test
+    void multicast() {
+        //given
+        Sinks.Many<Object> sink = Sinks.many().multicast().onBackpressureBuffer();
+
+        //when
+        Flux<Object> flux = sink.asFlux();
+
+        //then
+        flux.subscribe(Util.subscriber("art", latch));
+        IntStream.rangeClosed(1, 5).forEach(sink::tryEmitNext);
+        sink.tryEmitComplete();
+    }
+
+    @Test
+    void multicast_multipleSubscribers() {
+        //given
+        Sinks.Many<Object> sink = Sinks.many().multicast().onBackpressureBuffer();
+
+        //when
+        Flux<Object> flux = sink.asFlux();
+
+        //then
+        flux.subscribe(Util.subscriber("art", latch));
+        flux.subscribe(Util.subscriber("kate", latch));
+        IntStream.rangeClosed(1, 5).forEach(sink::tryEmitNext);
+        sink.tryEmitComplete();
+    }
+}
